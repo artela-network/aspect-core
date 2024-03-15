c9feb97d210b6527c704397ab2bf1e3ee9cb19d4
n06ody <4312182lyp@gmail.com>
2024-03-15 14:46:38 +0800

    fix: fix state db api issue

--- djpm/run/api/statedb_api.go
+++ djpm/run/api/statedb_api.go
@@ -10,63 +10,63 @@ import (
 func (r *Registry) stateDBAPIs() map[string]*types2.HostFuncWithGasRule {
 	return map[string]*types2.HostFuncWithGasRule{
 		"getBalance": {
-			Func: func(addr []byte) []byte {
+			Func: func(addr []byte) ([]byte, error) {
 				hook, err := types.GetStateDbHook(r.runnerContext.Ctx)
 				if err != nil || hook == nil {
 					panic("GetStateDbHook failed")
 				}
 				balance := hook.GetBalance(common.BytesToAddress(addr))
-				return wrapNilByte(balance.Bytes())
+				return wrapNilByte(balance.Bytes()), nil
 			},
 			GasRule: types2.NewStaticGasRule(2000),
 		},
 		"getState": {
-			Func: func(addr []byte, hash []byte) []byte {
+			Func: func(addr []byte, hash []byte) ([]byte, error) {
 				hook, err := types.GetStateDbHook(r.runnerContext.Ctx)
 				if err != nil || hook == nil {
 					panic("GetStateDbHook failed")
 				}
-				return wrapNilByte(hook.GetState(common.BytesToAddress(addr), common.BytesToHash(hash)).Bytes())
+				return wrapNilByte(hook.GetState(common.BytesToAddress(addr), common.BytesToHash(hash)).Bytes()), nil
 			},
 			GasRule: types2.NewStaticGasRule(5000),
 		},
 		"getCodeHash": {
-			Func: func(addr []byte) []byte {
+			Func: func(addr []byte) ([]byte, error) {
 				hook, err := types.GetStateDbHook(r.runnerContext.Ctx)
 				if err != nil || hook == nil {
 					panic("GetStateDbHook failed")
 				}
-				return wrapNilByte(hook.GetCodeHash(common.BytesToAddress(addr)).Bytes())
+				return wrapNilByte(hook.GetCodeHash(common.BytesToAddress(addr)).Bytes()), nil
 			},
 			GasRule: types2.NewStaticGasRule(40000),
 		},
 		"getCodeSize": {
-			Func: func(addr []byte) uint64 {
+			Func: func(addr []byte) (uint64, error) {
 				hook, err := types.GetStateDbHook(r.runnerContext.Ctx)
 				if err != nil || hook == nil {
 					panic("GetStateDbHook failed")
 				}
-				return uint64(hook.GetCodeSize(common.BytesToAddress(addr)))
+				return uint64(hook.GetCodeSize(common.BytesToAddress(addr))), nil
 			},
 			GasRule: types2.NewStaticGasRule(40000),
 		},
 		"getNonce": {
-			Func: func(addr []byte) uint64 {
+			Func: func(addr []byte) (uint64, error) {
 				hook, err := types.GetStateDbHook(r.runnerContext.Ctx)
 				if err != nil || hook == nil {
 					panic("GetStateDbHook failed")
 				}
-				return hook.GetNonce(common.BytesToAddress(addr))
+				return hook.GetNonce(common.BytesToAddress(addr)), nil
 			},
 			GasRule: types2.NewStaticGasRule(40000),
 		},
 		"hasSuicided": {
-			Func: func(addr []byte) bool {
+			Func: func(addr []byte) (bool, error) {
 				hook, err := types.GetStateDbHook(r.runnerContext.Ctx)
 				if err != nil || hook == nil {
 					panic("GetStateDbHook failed")
 				}
-				return hook.HasSuicided(common.BytesToAddress(addr))
+				return hook.HasSuicided(common.BytesToAddress(addr)), nil
 			},
 			GasRule: types2.NewStaticGasRule(40000),
 		},
