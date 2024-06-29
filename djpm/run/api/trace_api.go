5ea4fefa70a4525a0ac426b770d97f0caa0e7304
Jack Li <jack@artela.network>
2024-06-30 00:09:25 +0800

    fix: disable trace api for now

--- djpm/run/api/trace_api.go
+++ djpm/run/api/trace_api.go
@@ -24,20 +24,20 @@ func (r *Registry) traceAPIs() interface{} {
 			return wrapNilByte(hook.QueryStateChange(r.runnerContext, query))
 		},
 		"queryCallTree": func(rawQuery []byte) []byte {
-			hook, err := types.GetAspectTraceHostHook(r.runnerContext.Ctx)
-			if err != nil {
-				panic("failed to init aspect runtime context host api: " + err.Error())
-			}
-			if hook == nil {
-				panic("aspect runtime context host api not found")
-			}
+			//hook, err := types.GetAspectTraceHostHook(r.runnerContext.Ctx)
+			//if err != nil {
+			//	panic("failed to init aspect runtime context host api: " + err.Error())
+			//}
+			//if hook == nil {
+			//	panic("aspect runtime context host api not found")
+			//}
+			//
+			//query := &types.CallTreeQuery{}
+			//if err := proto.Unmarshal(rawQuery, query); err != nil {
+			//	panic("failed to unmarshal query: " + err.Error())
+			//}
 
-			query := &types.CallTreeQuery{}
-			if err := proto.Unmarshal(rawQuery, query); err != nil {
-				panic("failed to unmarshal query: " + err.Error())
-			}
-
-			return wrapNilByte(hook.QueryCallTree(r.runnerContext, query))
+			return wrapNilByte(nil)
 		},
 	}
 }
