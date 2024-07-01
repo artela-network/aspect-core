0c8bf870b1565675dc2c8058e11a1a7ede55c769
Jack Li <jack@artela.network>
2024-07-01 17:32:06 +0800

    fix: fix call tree data in-determinism issue

--- djpm/run/api/trace_api.go
+++ djpm/run/api/trace_api.go
@@ -35,25 +35,25 @@ func (r *Registry) traceAPIs() map[string]*types2.HostFuncWithGasRule {
 		},
 		"queryCallTree": {
 			Func: func(rawQuery []byte) ([]byte, error) {
-				//hook, err := types.GetAspectTraceHostHook(r.runnerContext.Ctx)
-				//if err != nil {
-				//	panic("failed to init aspect runtime context host api: " + err.Error())
-				//}
-				//if hook == nil {
-				//	panic("aspect runtime context host api not found")
-				//}
-				//
-				//query := &types.CallTreeQuery{}
-				//if err := proto.Unmarshal(rawQuery, query); err != nil {
-				//	return nil, errors.Wrap(err, "failed to unmarshal call tree query")
-				//}
-				//
-				//res, err := hook.QueryCallTree(r.runnerContext, query)
-				//if err != nil {
-				//	return nil, err
-				//}
+				hook, err := types.GetAspectTraceHostHook(r.runnerContext.Ctx)
+				if err != nil {
+					panic("failed to init aspect runtime context host api: " + err.Error())
+				}
+				if hook == nil {
+					panic("aspect runtime context host api not found")
+				}
 
-				return wrapNilByte(nil), nil
+				query := &types.CallTreeQuery{}
+				if err := proto.Unmarshal(rawQuery, query); err != nil {
+					return nil, errors.Wrap(err, "failed to unmarshal call tree query")
+				}
+
+				res, err := hook.QueryCallTree(r.runnerContext, query)
+				if err != nil {
+					return nil, err
+				}
+
+				return wrapNilByte(res), nil
 			},
 			GasRule: types2.NewStaticGasRule(1000000),
 		},
